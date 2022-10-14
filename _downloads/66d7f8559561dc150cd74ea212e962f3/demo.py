fed735f8cadc2cff6f7f481e303512cfb1193729
mgoulao <msilvagoulao@gmail.com>
2022-10-14 23:24:51 +0000

    Deploying to gh-pages from @ mgoulao/Gymnasium@b0a1f34b55de51d4b556536c3e086c6bcb27c3a0 🚀

--- _downloads/66d7f8559561dc150cd74ea212e962f3/demo.py
+++ _downloads/66d7f8559561dc150cd74ea212e962f3/demo.py
@@ -15,7 +15,7 @@ This file is not listed in the website and serves only to give an example of a t
 
 # This line won't be rendered as rST because there's a space after the last block.
 myvariable = 2
-print("my variable is {}".format(myvariable))
+print(f"my variable is {myvariable}")
 # This is the end of the 'code block' (if using an above IDE). All code within
 # this block can be easily executed all at once.
 
@@ -26,4 +26,4 @@ print("my variable is {}".format(myvariable))
 # In the built documentation, it will be rendered as rST after the code above!
 # This is also another code block.
 
-print(f'my variable plus 2 is {myvariable + 2}')
+print(f"my variable plus 2 is {myvariable + 2}")
